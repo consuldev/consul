75008347209c233930009d5f9adf00848eaea531
Melvin Lammerts <melvin@melvin.amsterdam>
2020-05-18 22:02:00 +0200

    Initial commit

--- config/initializers/i18n_translation.rb
+++ config/initializers/i18n_translation.rb
@@ -9,6 +9,10 @@ module ActionView
       def t(key, options = {})
         current_locale = options[:locale].presence || I18n.locale
 
+        # TODO: Implement DB lookup with https://github.com/svenfuchs/i18n-active_record
+        #       Or keep using Globalize which is already in the project and extend the functionality
+        #       of Admin::SiteCustomization::InformationTextsController
+
         i18_content = I18nContent.find_by(key: key)
         translation = I18nContentTranslation.find_by(i18n_content_id: i18_content&.id,
                                                      locale: current_locale)&.value
